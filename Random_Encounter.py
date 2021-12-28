3dd142ed1e4f04f633c2dc7fbcf53b161c0500ed
Patrick May <patjster@gmail.com>
2021-12-28 10:32:16 -0600

    First Python Upload

---
+++ Random_Encounter.py
@@ -0,0 +1,6 @@
+"""
+Random Encounter program
+- Will determine what kind of encounter the group faces if an encounter happens
+- Will provide monsters that will be dealt with in correlation to which country the group is in
+- Also will provide page numbers for the monsters for ease of use
+"""
