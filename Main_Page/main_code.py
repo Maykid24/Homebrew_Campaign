002b1c25658972df5a6c4a49dc55ecf6431dd39c
Patrick May <patjster@gmail.com>
2023-02-09 19:19:59 -0600

    Pushing out Monster list

---
+++ Main_Page/main_code.py
@@ -0,0 +1,8 @@
+"""
+Utilizing this page as a main page for python code
+"""
+
+import Homebrew_Campaign.Backend_Information.Monsters as Monsters
+
+for key, value in Monsters.artic.items():
+    print(key, value)
